0c38a3d9fab7487a49e2ed426a4fc77644956b28
jameslazo <james.lazo+git@protonmail.com>
2025-01-09 07:03:28 -0700

    init tf repo

---
+++ vars.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  default = ""
+}
+
+variable "vpc_cidr_block" {
+  default = ""
+}
+
+variable "bucket_name" {
+  default = ""
+}
+
+variable "bucket_tags" {
+  default = {
+    Name = ""
+  }
+}
+
+variable "role_name" {
+  default = ""
+}
+
+variable "account_id" {
+  default = ""
+}
+
+variable "repo_name" {
+  default = ""
+}
