3db9d245107311f1f63bbe6d2b28522a55adbc57
James Lazo <james.lazo+git@protonmail.com>
2025-02-11 07:19:11 -0700

    chore: refactor variables.tf to vars.tf for consistency, pull shared state into security.tf for shared vpc id

--- day-05/security.tf
+++ day-05/security.tf
@@ -1,7 +1,40 @@
+/************************************
+* data from shared tf state outputs *
+*************************************
+  shared_state:
+    outputs:
+      vpc:
+        aws_vpc:
+          aws_vpc_id: "ID"
+      ddb:
+        aws_dynamodb_table:
+          keys: 
+            - day0{1,2,3,4,5,6}
+      s3:
+        aws_s3_bucket:
+          lambda_bucket: "ID"
+      iam:
+        aws_iam_role:
+          keys:
+            - lambda_exec{.arn,.name}
+        aws_iam_role_policy_attachment:
+          lambda_sns_publish_attachment: "ID"
+************************************/
+
+data "terraform_remote_state" "shared_state" {
+  backend = "s3"
+  config = {
+    bucket = "${var.devops_backend_bucket}" # S3 bucket storing the source state
+    key    = "shared/terraform.tfstate"     # Path to the source state file
+    region = "${var.region}"
+  }
+}
+
+
 resource "aws_security_group" "ecs_task" {
   name        = "${var.project_name}-ecs-task-sg"
   description = "Security group for ECS tasks"
-  vpc_id      = var.vpc_id
+  vpc_id      = data.terraform_remote_state.shared_state.outputs.aws_vpc_id
 
   ingress {
     from_port   = 443
